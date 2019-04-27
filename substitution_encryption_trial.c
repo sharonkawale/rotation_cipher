1a5b0f3fdfc4df4b8174b557bdac097c4be3df5d
Sharon Kawale <c3297125@uon.edu.au>
2019-04-27 19:42:46 +0000

    transformation substitution function partially works

--- substitution_encryption_trial.c
+++ substitution_encryption_trial.c
@@ -1,37 +1,31 @@
 #include <stdio.h> 
 
-void transform(char *alphabet);
+void transform(char* alphabet);
 
 
 int main() {
     
-    char alpahabet[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    
-
-
-char plain_text = 'MY  NAME IS SHARON'
-    
+   
+    char alphabet[3] = {'a','b','c'};
     
  
+    transform(alphabet);
     
-    
-    while(feof(input) ==0 ) {
-        
-       fscanf(input, "%c", &inchar);
-       
-       key = 'qwertyuiopasdfghjklzxcvbnm' ;
-       
-}
+    printf("%c", *alphabet);
 }
 
 
 void transform(char *alphabet) {
     
-    int i;
-    for (i = 0; i<26; i++);
+    int i ;
     
+    char key[3] = {'q','w','e'};
     
+    for (i = 0; i<4; i++) {
+        alphabet[i] = key[i];
+    }
     
+    return;    
 }
 
 
