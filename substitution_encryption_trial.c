68ca8d99caf3136f759435450b19826e046e8f87
Sharon Kawale <c3297125@uon.edu.au>
2019-04-27 18:11:34 +0000

    substitution function

---
+++ substitution_encryption_trial.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+
+void transform(char *alphabet);
+
+
+int main() {
+    
+    char alpahabet[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    
+
+
+char plain_text = 'MY  NAME IS SHARON'
+    
+    
+ 
+    
+    
+    while(feof(input) ==0 ) {
+        
+       fscanf(input, "%c", &inchar);
+       
+       key = 'qwertyuiopasdfghjklzxcvbnm' ;
+       
+}
+}
+
+
+void transform(char *alphabet) {
+    
+    int i;
+    for (i = 0; i<26; i++);
+    
+    
+    
+}
+
+
+
