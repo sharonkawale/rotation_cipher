7854a2b80c203bc1e9e70eda448d87c435e0e298
Sharon Kawale <c3297125@uon.edu.au>
2019-04-28 00:10:51 +0000

    improved  substitution transformation of alphabets

--- substitution_encryption_trial.c
+++ substitution_encryption_trial.c
@@ -1,40 +1,48 @@
 #include <stdio.h> 
 
-void transform(char *alphabet, char *key);
+char transform(char *alphabet, char *key);
 
 
 int main() {
     
       int i ;
    
-    char alphabet[] = {'a','b','c'};
-    char key[] = {'q','w','e'};
- 
- 
-    transform(alphabet,key);
- 
+    char alphabet[26] ;
+    char key[26] = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
     
-    for (i = 0; i<3; i++) {
+    for (i=0;i<26;i++){
+        alphabet[i] = 'A'+i;
+        transform(alphabet,key);
+        
+        printf("%c", alphabet[i]);
+    }
+ 
+ 
+ /*for (i = 0; i<26; i++) {
+        transform(alphabet,key);
         
         printf("%c", alphabet[i]);
         
-    } 
+    } */
     
 }
 
 
-void transform(char *alphabet, char*key) {
+char transform(char *alphabet, char*key) {
     
     int i ;
     
  
     
-    for (i = 0; i<3; i++) {
+    for (i = 0; i<26; i++) {
         alphabet[i] = key[i];
     }
     
-    return ;   
+    return *alphabet;   
 }
+
+
+
 
 
 
